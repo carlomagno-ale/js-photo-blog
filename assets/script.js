4f455dd716f319abbabb14cd07dbb118a96e3fa2
carlomagno-ale <carlomagno.ale@gmail.com>
2025-02-28 15:42:40 +0100

    add test ajax call

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,6 @@
-console.log('hello')
+//chiamata ajax di test
+axios.get("https://lanciweb.github.io/demo/api/pictures")
+.then(response => {
+   console.log(response.data)
+})
+.catch(error => console.error(error));
