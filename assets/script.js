4f32961fc0c4846de7b1b974ac18e6b34dba4bca
carlomagno-ale <carlomagno.ale@gmail.com>
2025-02-28 16:05:17 +0100

    get 1 card information

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,28 @@
-//chiamata ajax di test
+const photoList = document.getElementById("photo-list")
+
+//chiamata ajax
+
 axios.get("https://lanciweb.github.io/demo/api/pictures")
-.then(response => {
-   console.log(response.data)
-})
-.catch(error => console.error(error));
+   .then(response => {
+
+      const img = response.data[0].url
+      console.log(img)
+      const date = response.data[0].date
+      console.log(date)
+      const title = response.data[0].title
+
+      photoList.innerHTML = ` 
+     <div class="col">
+          <div class="card">
+            <div class="pin"></div>
+            <img src="${img}" alt="spiaggia">
+            <div class="title-card">
+              <span>${date}</span>
+              <h2>${title}</h2>
+            </div>
+          </div>
+        </div>  `
+
+
+   })
+   .catch(error => console.error(error));
