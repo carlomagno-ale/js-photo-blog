ff850aa6e828cb956e6e8358f4c999f7955943c2
carlomagno-ale <carlomagno.ale@gmail.com>
2025-03-03 12:41:09 +0100

    add selection single card with click

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,21 @@
 const photoList = document.getElementById("photo-list")
 const urlEndpoint = "https://lanciweb.github.io/demo/api/pictures"
 
-
 //ajax call to generate the cards with various data 
 axios.get(urlEndpoint)
-   .then(response => {
+  .then(response => {
 
-      for (let i = 0; i < response.data.length; i++) {
-         const photoData = response.data[i];
+    for (let i = 0; i < response.data.length; i++) {
+      const photoData = response.data[i];
 
-         const img = photoData.url
-         console.log(img)
-         const date = photoData.date
-         console.log(date)
-         const title = photoData.title
-         console.log(title)
+      const img = photoData.url
+      console.log(img)
+      const date = photoData.date
+      console.log(date)
+      const title = photoData.title
+      console.log(title)
 
-         photoList.innerHTML += ` 
+      photoList.innerHTML += ` 
          <div class="col-md-6 col-lg-4 col-sm-6 col-12 mt-4">
           <div class="card rounded-0 border-0">
             <div class="pin"></div>
@@ -27,8 +26,20 @@ axios.get(urlEndpoint)
             </div>
           </div>
         </div>  `
-      }
+    }
 
-   })
-   .catch(error => console.error(error));
+    const allCard = document.querySelectorAll(".card")
+    console.log(allCard)
+
+    for (let i = 0; i < allCard.length; i++) {
+
+      allCard[i].addEventListener('click', funzione1)
+
+      function funzione1() {
+        allCard[i].style.backgroundColor = "red";
+      }
+    }
+  })
+  .catch(error => console.error(error));
+
 
